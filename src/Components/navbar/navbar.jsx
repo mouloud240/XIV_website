a39da31500c8ea0ab97093a29ca1f4ccef944062
mouloud240 <m.hasrane@esi-sba.dz>
2024-05-02 20:48:40 +0100

    Almost finished navbar+font added

--- src/Components/navbar/navbar.jsx
+++ src/Components/navbar/navbar.jsx
@@ -1,8 +1,80 @@
+import Navlinks from "@/Constants"
+import Image from "next/image"
+import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <div>
-        
+    <div className="mt-4 mx-10 ">
+        <div className="flex justify-between items-center">
+            <ul className="flex gap-6 items-center">
+                <button>
+                    <Image
+                    src={'Assets/drawer.svg'}
+                    width={35}
+                    height={45}
+                    alt="Drawer"
+                    />
+                </button>
+                {
+                    Navlinks.map((item,index)=>(
+                        <li key={index}>
+                           <Link href={item.path}>
+                            <p className="font-Beatrice-Deck-Trial font-medium">
+                            {item.name}
+                            </p>
+                          
+                           </Link>
+                        </li>
+
+                    ))
+                }
+                 
+            </ul>
+            <div>
+                <Image
+                src={'Assets/Logo.svg'}
+                width={45}
+                height={45}
+                
+                />
+            </div>
+            <div className="flex gap-10 ">
+                <div className="bg-black rounded-full flex justify-center  px-4 items-center bg-opacity-80 ">
+                    <Image
+                    src={'Assets/heart.svg'}
+                    width={25}
+                    height={25}
+                    alt="Heart"
+                    />
+                      
+                </div>
+                <div className="flex ">
+                    <div className="bg-black rounded-full flex items-center justify-center  py-1 px-8  bg-opacity-80">
+                          <p className="text-white font-medium">
+                            Cart
+                          </p>
+                        
+                    </div>
+                    <div className="border-8 border-black  rounded-full border-opacity-80">
+                    <Image
+                          src={'Assets/bag.svg'}
+                          height={45}
+                          width={60}
+                          alt="Bag"
+                          />
+                    </div>
+                </div>
+                <div className="bg-black bg-opacity-80 rounded-full items-center flex justify-center px-7">
+                    <Image
+                    src={'Assets/profile.svg'}
+                    height={25}
+                    width={25}
+                    alt="Profile"
+                    />
+                </div>
+            </div>
+
+        </div>
     </div>
   )
 }
