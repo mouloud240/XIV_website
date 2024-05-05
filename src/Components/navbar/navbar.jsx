9b770b57040770042de6eb713741ef8fe5b7934d
mouloud240 <m.hasrane@esi-sba.dz>
2024-05-05 14:38:57 +0100

    navbar fixes

--- src/Components/navbar/navbar.jsx
+++ src/Components/navbar/navbar.jsx
@@ -8,6 +8,7 @@ const Navbar = () => {
         <div className="flex justify-between items-center">
             <ul className="flex gap-6 items-center">
                 <button>
+                    
                     <Image
                     src={'Assets/drawer.svg'}
                     width={35}
@@ -31,31 +32,37 @@ const Navbar = () => {
                  
             </ul>
             <div>
+                <Link href={'/'}>
                 <Image
                 src={'Assets/Logo.svg'}
                 width={45}
                 height={45}
                 
                 />
+                </Link>
+               
             </div>
             <div className="flex gap-10 ">
-                <div className="bg-black rounded-full flex justify-center  px-4 items-center bg-opacity-80 ">
-                    <Image
-                    src={'Assets/heart.svg'}
-                    width={25}
-                    height={25}
-                    alt="Heart"
-                    />
-                      
-                </div>
+            
+                    <div className="bg-black rounded-full flex justify-center  px-6 items-center bg-opacity-80 cursor-pointer ">
+                        
+                        <Image
+                        src={'Assets/heart.svg'}
+                        width={25}
+                        height={25}
+                        alt="Heart"
+                        />
+                          
+                    </div>
+          
                 <div className="flex ">
-                    <div className="bg-black rounded-full flex items-center justify-center  py-1 px-8  bg-opacity-80">
+                    <div className="bg-black rounded-full flex items-center justify-center  py-1 px-8  bg-opacity-80 cursor-pointer">
                           <p className="text-white font-medium">
                             Cart
                           </p>
                         
                     </div>
-                    <div className="border-8 border-black  rounded-full border-opacity-80">
+                    <div className="border-8 border-black  rounded-full border-opacity-80 cursor-pointer">
                     <Image
                           src={'Assets/bag.svg'}
                           height={45}
@@ -64,7 +71,7 @@ const Navbar = () => {
                           />
                     </div>
                 </div>
-                <div className="bg-black bg-opacity-80 rounded-full items-center flex justify-center px-7">
+                <div className="bg-black bg-opacity-80 rounded-full items-center flex justify-center px-7 cursor-pointer">
                     <Image
                     src={'Assets/profile.svg'}
                     height={25}
